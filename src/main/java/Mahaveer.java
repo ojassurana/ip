9c7a6baf04a6cd05f03a86af9334fd1476f082bc
ojassurana <me@ojassurana.com>
2025-01-24 11:19:28 +0800

    Changed name to Mahaveer and implemented greeting feature

---
+++ src/main/java/Mahaveer.java
@@ -0,0 +1,6 @@
+public class Mahaveer {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________\n";
+        System.out.println(line+" Hello! I'm Mahaveer\n What can I do for you?\n"+line+" Bye. Hope to see you again soon!\n"+line);
+    }
+}
