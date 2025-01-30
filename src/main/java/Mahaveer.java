90470c46a33110830988332030b091c9a93aaad5
ojassurana <me@ojassurana.com>
2025-01-30 20:25:50 +0800

    Implemented basic echo feature.

--- src/main/java/Mahaveer.java
+++ src/main/java/Mahaveer.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Mahaveer {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String toEcho;
         String line = "____________________________________________________________\n";
-        System.out.println(line+" Hello! I'm Mahaveer\n What can I do for you?\n"+line+" Bye. Hope to see you again soon!\n"+line);
+        System.out.println(line+" Hello! I'm Mahaveer\n What can I do for you?\n"+line);
+        boolean notBye = true;
+        while (notBye){
+            toEcho = in.nextLine();
+            System.out.println("----");
+            if (toEcho.equals("bye")){
+                notBye = false;
+            }
+            else {
+                System.out.println(toEcho);
+                System.out.println("----");
+            }
+        }
+        System.out.println(line);
+        System.out.println("Jai Jinendra! Till we meet next time :)");
     }
 }
