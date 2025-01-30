236dfbba2081f6f4b6500e4b877043eb40944487
ojassurana <me@ojassurana.com>
2025-01-30 21:21:10 +0800

    Reformatted the code to respect the coding standard.

--- src/main/java/Mahaveer.java
+++ src/main/java/Mahaveer.java
@@ -7,15 +7,14 @@ public class Mahaveer {
         String toEcho;
         int counter = 0;
         String line = "____________________________________________________________\n";
-        System.out.println(line+" Hello! I'm Mahaveer\n What can I do for you?\n"+line);
+        System.out.println(line + " Hello! I'm Mahaveer\n What can I do for you?\n" + line);
         boolean notBye = true;
-        while (notBye){
+        while (notBye) {
             toEcho = in.nextLine();
             System.out.println("----");
-            if (toEcho.equals("bye")){
+            if (toEcho.equals("bye")) {
                 notBye = false;
-            }
-            else if (toEcho.equals("list")){
+            } else if (toEcho.equals("list")) {
                 if (counter == 0) {
                     System.out.println("No tasks available.");
                 } else {
@@ -25,11 +24,10 @@ public class Mahaveer {
                     }
                 }
                 System.out.println("----");
-            }
-            else if (toEcho.startsWith("mark ")){
+            } else if (toEcho.startsWith("mark ")) {
                 try {
                     int taskNumber = Integer.parseInt(toEcho.substring(5)) - 1;
-                    if(taskNumber >= 0 && taskNumber < counter){
+                    if (taskNumber >= 0 && taskNumber < counter) {
                         Task task = taskList[taskNumber];
                         task.setDone(true);
                         System.out.println("Nice! I've marked this task as done:");
@@ -37,15 +35,14 @@ public class Mahaveer {
                     } else {
                         System.out.println("Invalid task number.");
                     }
-                } catch(NumberFormatException e){
+                } catch (NumberFormatException e) {
                     System.out.println("Please provide a valid task number.");
                 }
                 System.out.println("----");
-            }
-            else if (toEcho.startsWith("unmark ")){
+            } else if (toEcho.startsWith("unmark ")) {
                 try {
                     int taskNumber = Integer.parseInt(toEcho.substring(7)) - 1;
-                    if(taskNumber >= 0 && taskNumber < counter){
+                    if (taskNumber >= 0 && taskNumber < counter) {
                         Task task = taskList[taskNumber];
                         task.setDone(false);
                         System.out.println("OK, I've marked this task as not done yet:");
@@ -53,12 +50,11 @@ public class Mahaveer {
                     } else {
                         System.out.println("Invalid task number.");
                     }
-                } catch(NumberFormatException e){
+                } catch (NumberFormatException e) {
                     System.out.println("Please provide a valid task number.");
                 }
                 System.out.println("----");
-            }
-            else {
+            } else {
                 System.out.println("Stored: " + toEcho);
                 System.out.println("----");
                 taskList[counter] = new Task(toEcho);
