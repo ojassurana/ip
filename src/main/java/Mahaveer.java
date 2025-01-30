c443adafac667e9604bbe41273395f64eb559e2f
ojassurana <me@ojassurana.com>
2025-01-30 20:38:35 +0800

    Implemented the ability for the chatbot to 'store' texts and added edit the text.

--- src/main/java/Mahaveer.java
+++ src/main/java/Mahaveer.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class Mahaveer {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String[] taskList = new String[100];
         String toEcho;
+        int counter = 0;
         String line = "____________________________________________________________\n";
         System.out.println(line+" Hello! I'm Mahaveer\n What can I do for you?\n"+line);
         boolean notBye = true;
@@ -14,8 +16,10 @@ public class Mahaveer {
                 notBye = false;
             }
             else {
-                System.out.println(toEcho);
+                System.out.println("Stored: " + toEcho);
                 System.out.println("----");
+                taskList[counter] = toEcho;
+                counter++;
             }
         }
         System.out.println(line);
