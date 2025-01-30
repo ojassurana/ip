0011ccdc663c558f07cc8dc44a3b394132680fa8
ojassurana <me@ojassurana.com>
2025-01-30 21:12:34 +0800

    Added the ability to mark tasks.

--- src/main/java/Mahaveer.java
+++ src/main/java/Mahaveer.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Mahaveer {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String[] taskList = new String[100];
+        Task[] taskList = new Task[100];
         String toEcho;
         int counter = 0;
         String line = "____________________________________________________________\n";
@@ -19,16 +19,33 @@ public class Mahaveer {
                 if (counter == 0) {
                     System.out.println("No tasks available.");
                 } else {
+                    System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < counter; i++) {
-                        System.out.println((i + 1) + ". " + taskList[i]);
+                        System.out.println(" " + (i + 1) + ". [" + taskList[i].getStatusIcon() + "] " + taskList[i].description);
                     }
+                }
+                System.out.println("----");
+            }
+            else if (toEcho.startsWith("mark ")){
+                try {
+                    int taskNumber = Integer.parseInt(toEcho.substring(5)) - 1;
+                    if(taskNumber >= 0 && taskNumber < counter){
+                        Task task = taskList[taskNumber];
+                        task.setDone(true);
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("[" + task.getStatusIcon() + "] " + task.description);
+                    } else {
+                        System.out.println("Invalid task number.");
+                    }
+                } catch(NumberFormatException e){
+                    System.out.println("Please provide a valid task number.");
                 }
                 System.out.println("----");
             }
             else {
                 System.out.println("Stored: " + toEcho);
                 System.out.println("----");
-                taskList[counter] = toEcho;
+                taskList[counter] = new Task(toEcho);
                 counter++;
             }
         }
