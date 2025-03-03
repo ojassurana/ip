4717531a431b78b1ce1d7c6bc43aa6e16889808a
ojassurana <me@ojassurana.com>
2025-03-03 14:59:23 +0800

    Completed coding Mahaveer.java class to work with Parser.java.

--- src/main/java/mahaveer/Mahaveer.java
+++ src/main/java/mahaveer/Mahaveer.java
@@ -1,6 +1,6 @@
 package mahaveer;
-
 import mahaveer.exception.MaheveerException;
+
 import mahaveer.task.Deadline;
 import mahaveer.task.Event;
 import mahaveer.task.Task;
@@ -8,57 +8,7 @@ import mahaveer.task.Task;
 import java.util.List;
 import java.util.ArrayList;
 
-
 public class Mahaveer {
-
-    private static void emptyDescription(String userInput, String commandWord) throws MaheveerException {
-        userInput = userInput.trim();
-        int minLengthRequired;
-        String expectedPrefix = commandWord + " ";
-
-        // Determine the minimum length to allow for actual description text
-        String commandHint = switch (commandWord) {
-            case "todo" -> {
-                minLengthRequired = 5;
-                yield """
-                        A 'todo' requires a short description.
-                        For example:
-                          todo Bake a cake
-                          todo Walk the dog
-                        """;
-            }
-            case "deadline" -> {
-                minLengthRequired = 9; // "deadline " -> 9 chars
-                yield """
-                        A 'deadline' requires a description and '/by' time.
-                        For example:
-                          deadline Submit assignment /by tonight
-                          deadline Finish reading /by next Monday
-                        """;
-            }
-            case "event" -> {
-                minLengthRequired = 6; // "event " -> 6 chars
-                yield """
-                        An 'event' requires a description plus '/from' and '/to'.
-                        For example:
-                          event Conference /from Monday /to Wednesday
-                          event Birthday party /from 2pm /to 6pm
-                        """;
-            }
-            default -> {
-                minLengthRequired = commandWord.length() + 1;
-                yield "Please provide more details for the command '" + commandWord + "'.";
-            }
-        };
-
-        // If the input doesn't start with <command> + space
-        // OR the user typed no additional characters, throw exception
-        if (!userInput.startsWith(expectedPrefix) || userInput.length() <= minLengthRequired) {
-            throw new MaheveerException(
-                    "The description of '" + commandWord + "' cannot be empty :c\n" + commandHint
-            );
-        }
-    }
 
     public static void main(String[] args) {
         Ui ui = new Ui();
@@ -71,141 +21,133 @@ public class Mahaveer {
         ui.showWelcome();
         boolean notBye = true;
         while (notBye) {
-            String toEcho = ui.readCommand();
-            ui.showLine();
-            if (toEcho.equals("bye")) {
-                notBye = false;
+            try {
+                Parser.ParsedCommand commandDetails = Parser.getCommandDetails();
+                ui.showLine();
 
-            } else if (toEcho.equals("list")) {
-                if (taskList.isEmpty()) {
-                    ui.showToUser("No tasks available.");
+                if (commandDetails.command().equals("bye")) {
+                    notBye = false;
                 } else {
-                    ui.showToUser("Here are the tasks in your list:");
-                    for (int i = 0; i < taskList.size(); i++) {
-                        ui.showToUser(" " + (i + 1) + ". " + taskList.get(i));
-                    }
-                }
-                ui.showLine();
-
-            } else if (toEcho.startsWith("mark")) {
-                try {
-                    int taskNumber = Integer.parseInt(toEcho.substring(5).trim()) - 1;
-                    if (taskNumber >= 0 && taskNumber < taskList.size()) {
-                        Task task = taskList.get(taskNumber);
-                        if (task.isDone()) {
-                            ui.showToUser("This task is already marked as done!");
+                    switch (commandDetails.command()) {
+                    case "list":
+                        if (taskList.isEmpty()) {
+                            ui.showToUser("No tasks available.");
                         } else {
-                            storage.markTask(taskNumber);
-                            task.setDone(true);
-                            ui.showToUser("Nice! I've marked this task as done:");
-                            ui.showToUser("  [" + task.getStatusIcon() + "] " + task.getDescription());
+                            ui.showToUser("Here are the tasks in your list:");
+                            for (int i = 0; i < taskList.size(); i++) {
+                                ui.showToUser(" " + (i + 1) + ". " + taskList.get(i));
+                            }
                         }
-                    } else {
-                        ui.showToUser("Task number does not exist.");
-                    }
-                } catch (Exception e) {
-                    ui.showToUser("Please provide a valid task number.");
-                }
-                ui.showLine();
-
-            } else if (toEcho.startsWith("unmark")) {
-                try {
-                    int taskNumber = Integer.parseInt(toEcho.substring(7).trim()) - 1;
-                    if (taskNumber >= 0 && taskNumber < taskList.size()) {
-                        Task task = taskList.get(taskNumber);
-                        if (!task.isDone()) {
-                            ui.showToUser("This task is already marked as not done!");
+                        break;
+                    case "mark":
+                        Integer markNumber = commandDetails.number();
+                        if (markNumber == null) {
+                            ui.showToUser("Please provide a valid task number.");
                         } else {
-                            task.setDone(false);
-                            ui.showToUser("OK, I've marked this task as not done yet:");
-                            ui.showToUser("  [" + task.getStatusIcon() + "] " + task.getDescription());
-                            storage.unmarkTask(taskNumber);
+                            int taskIndex = markNumber - 1;
+                            if (taskIndex >= 0 && taskIndex < taskList.size()) {
+                                Task task = taskList.get(taskIndex);
+                                if (task.isDone()) {
+                                    ui.showToUser("This task is already marked as done!");
+                                } else {
+                                    storage.markTask(taskIndex);
+                                    task.setDone(true);
+                                    ui.showToUser("Nice! I've marked this task as done:");
+                                    ui.showToUser("  [" + task.getStatusIcon() + "] " + task.getDescription());
+                                }
+                            } else {
+                                ui.showToUser("Task number does not exist.");
+                            }
                         }
-                    } else {
-                        ui.showToUser("Task number does not exist.");
+                        break;
+                    case "unmark":
+                        Integer unmarkNumber = commandDetails.number();
+                        if (unmarkNumber == null) {
+                            ui.showToUser("Please provide a valid task number.");
+                        } else {
+                            int taskIndex = unmarkNumber - 1;
+                            if (taskIndex >= 0 && taskIndex < taskList.size()) {
+                                Task task = taskList.get(taskIndex);
+                                if (!task.isDone()) {
+                                    ui.showToUser("This task is already marked as not done!");
+                                } else {
+                                    task.setDone(false);
+                                    ui.showToUser("OK, I've marked this task as not done yet:");
+                                    ui.showToUser("  [" + task.getStatusIcon() + "] " + task.getDescription());
+                                    storage.unmarkTask(taskIndex);
+                                }
+                            } else {
+                                ui.showToUser("Task number does not exist.");
+                            }
+                        }
+                        break;
+                    case "todo":
+                        String todoDesc = commandDetails.task();
+                        if (todoDesc.isEmpty()) {
+                            ui.showToUser(
+                                    "A 'todo' requires a short description.\nFor example:\n  todo Bake a cake\n  todo Walk the dog");
+                        } else {
+                            taskList.add(new Task(todoDesc));
+                            ui.showToUser("Got it. I've added this task:");
+                            ui.showToUser("  [T][ ] " + todoDesc);
+                            storage.addTodo(todoDesc);
+                        }
+                        break;
+                    case "deadline":
+                        String deadlineTask = commandDetails.task();
+                        String by = commandDetails.extra();
+                        if (deadlineTask.isEmpty() || by == null || by.isEmpty()) {
+                            ui.showToUser(
+                                    "A 'deadline' requires a description and '/by' time.\nFor example:\n  deadline Submit assignment /by tonight\n  deadline Finish reading /by next Monday");
+                        } else {
+                            taskList.add(new Deadline(deadlineTask, by));
+                            ui.showToUser("Got it. I've added this task:");
+                            ui.showToUser("  " + taskList.get(taskList.size() - 1).toString());
+                            storage.addDeadline(deadlineTask, by);
+                        }
+                        break;
+                    case "event":
+                        String eventTask = commandDetails.task();
+                        String from = commandDetails.extra();
+                        String to = commandDetails.extra2();
+                        if (eventTask.isEmpty() || from == null || from.isEmpty() || to == null || to.isEmpty()) {
+                            ui.showToUser(
+                                    "An 'event' requires a description plus '/from' and '/to'.\nFor example:\n  event Conference /from Monday /to Wednesday\n  event Birthday party /from 2pm /to 6pm");
+                        } else {
+                            taskList.add(new Event(eventTask, from, to));
+                            ui.showToUser("Got it. I've added this task:");
+                            ui.showToUser("  " + taskList.get(taskList.size() - 1).toString());
+                            storage.addEvent(eventTask, from, to);
+                        }
+                        break;
+                    case "delete":
+                        Integer deleteNumber = commandDetails.number();
+                        if (deleteNumber == null) {
+                            ui.showToUser("Please provide a valid task number.");
+                        } else {
+                            int taskIndex = deleteNumber - 1;
+                            if (taskIndex >= 0 && taskIndex < taskList.size()) {
+                                Task removedTask = taskList.remove(taskIndex);
+                                ui.showToUser("Noted. I've removed this task:");
+                                ui.showToUser("  " + removedTask);
+                                ui.showToUser("Now you have " + taskList.size() + " tasks in the list.");
+                                storage.deleteTask(taskIndex);
+                            } else {
+                                ui.showToUser("Task number does not exist.");
+                            }
+                        }
+                        break;
+                    default:
+                        ui.showToUser(
+                                "I'm sorry, I don't understand what you want me to do :c\nPlease refer to mahaveer.Mahaveer Manual! (COMING SOON ON README.md)");
+                        break;
                     }
-                } catch (Exception e) {
-                    ui.showToUser("Please provide a valid task number.");
                 }
-                ui.showLine();
-
-            } else if (toEcho.startsWith("todo")) {
-                try {
-                    emptyDescription(toEcho, "todo");
-                    String description = toEcho.substring(5).trim();
-                    taskList.add(new Task(description));
-                    ui.showToUser("Got it. I've added this task:");
-                    ui.showToUser("  [T][ ] " + description);
-                    storage.addTodo(description);
-                } catch (MaheveerException e) {
-                    ui.showToUser(e.getMessage());
-                }
-
-            } else if (toEcho.startsWith("deadline")) {
-                try {
-                    emptyDescription(toEcho, "deadline");
-                    String[] parts = toEcho.substring(9).split(" /by ");
-                    if (parts.length < 2) {
-                        throw new MaheveerException("Invalid deadline format. Use: deadline <description> /by <time>");
-                    } else {
-                        String description = parts[0].trim();
-                        String by = parts[1].trim();
-                        taskList.add(new Deadline(description, by));
-                        ui.showToUser("Got it. I've added this task:");
-                        ui.showToUser("  " + taskList.get(taskList.size() - 1).toString());
-                        storage.addDeadline(description, by);
-                    }
-                } catch (MaheveerException e) {
-                    ui.showToUser(e.getMessage());
-                }
-
-            } else if (toEcho.startsWith("event")) {
-                try {
-                    emptyDescription(toEcho, "event");
-                    String[] parts = toEcho.substring(6).split(" /from | /to ");
-                    if (parts.length < 3) {
-                        throw new MaheveerException("Invalid event format. Use: event <description> /from <start> /to <end>");
-                    } else {
-                        String description = parts[0].trim();
-                        String start = parts[1].trim();
-                        String end = parts[2].trim();
-                        taskList.add(new Event(description, start, end));
-                        ui.showToUser("Got it. I've added this task:");
-                        ui.showToUser("  " + taskList.get(taskList.size() - 1).toString());
-                        storage.addEvent(description, start, end);
-                    }
-                } catch (MaheveerException e) {
-                    ui.showToUser(e.getMessage());
-                }
-            } else if (toEcho.startsWith("delete")) {
-                try {
-                    int taskNumber = Integer.parseInt(toEcho.substring(7).trim()) - 1;
-                    if (taskNumber >= 0 && taskNumber < taskList.size()) {
-                        // Get the task before removing, so we can show it to the user
-                        Task removedTask = taskList.remove(taskNumber);
-                        ui.showToUser("Noted. I've removed this task:");
-                        ui.showToUser("  " + removedTask);
-                        ui.showToUser("Now you have " + taskList.size() + " tasks in the list.");
-                        storage.deleteTask(taskNumber);
-                    } else {
-                        ui.showToUser("Task number does not exist.");
-                    }
-                } catch (Exception e) {
-                    ui.showToUser("Please provide a valid task number.");
-                }
-            } else {
-                try {
-                    throw new MaheveerException(
-                            "I'm sorry, I don't understand what you want me to do :c\n"
-                                    + "Please refer to mahaveer.Mahaveer Manual! (COMING SOON ON README.md)"
-                    );
-                } catch (MaheveerException e) {
-                    ui.showToUser(e.getMessage());
-                }
+            } catch (MaheveerException e) {
+                ui.showToUser(e.getMessage());
             }
+            ui.showLine();
         }
-
-        ui.showLine();
         ui.showToUser("Jai Jinendra! Till we meet next time :)");
     }
 }
