fe8bec98964af4c126cea670f94023ce8459a7f9
ojassurana <me@ojassurana.com>
2025-03-04 23:36:26 +0800

    Added Javadoc for all FindCommand function.

--- src/main/java/mahaveer/command/FindCommand.java
+++ src/main/java/mahaveer/command/FindCommand.java
@@ -8,13 +8,38 @@ import mahaveer.Storage;
 import mahaveer.task.Task;
 import mahaveer.exception.MaheveerException;
 
+/**
+ * The FindCommand class handles searching for tasks that match a specified query.
+ * <p>
+ * When executed, it filters tasks in the task list by checking if each task's description
+ * contains the given query (case-insensitive) and displays the matching tasks to the user.
+ * </p>
+ */
 public class FindCommand extends Command {
     private final String query;
 
+    /**
+     * Constructs a new FindCommand with the specified search query.
+     *
+     * @param query the query string used to search for matching tasks
+     */
     public FindCommand(String query) {
         this.query = query;
     }
 
+    /**
+     * Executes the find command.
+     * <p>
+     * This method iterates over all tasks in the task list, collects tasks whose descriptions
+     * contain the query (ignoring case), and displays either the matching tasks or a message
+     * if no matches are found.
+     * </p>
+     *
+     * @param tasks   the current task list
+     * @param ui      the user interface for displaying messages
+     * @param storage the storage system (not used by this command)
+     * @throws MaheveerException if an error occurs during execution
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws MaheveerException {
         List<Task> matchingTasks = new ArrayList<>();
